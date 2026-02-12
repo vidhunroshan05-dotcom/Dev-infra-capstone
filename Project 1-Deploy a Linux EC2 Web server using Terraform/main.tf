b27eb877eeb3bc166864ea91c2f10de5d074edde
vidhun <vidhunroshan05@gmail.com>
2026-02-12 17:00:21 +0530

    project

--- Project 1-Deploy a Linux EC2 Web server using Terraform/main.tf
+++ Project 1-Deploy a Linux EC2 Web server using Terraform/main.tf
@@ -1,12 +1,4 @@
-data "aws_ami" "amazon_linux" {
-  most_recent = true
-  owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
 
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
--- Project 18-GitOps Infra deployment/main.tf
+++ Project 18-GitOps Infra deployment/main.tf
@@ -1,1 +1,1 @@
-#gitops testing
+#gitops testing CI/CD
