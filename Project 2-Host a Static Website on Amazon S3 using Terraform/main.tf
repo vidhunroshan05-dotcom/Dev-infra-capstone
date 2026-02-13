15b58bb12dc5715851288ad968d9baac1803dec0
vidhun <vidhunroshan05@gmail.com>
2026-02-13 10:52:14 +0530

    project 2

--- Project 2-Host a Static Website on Amazon S3 using Terraform/main.tf
+++ Project 2-Host a Static Website on Amazon S3 using Terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "static_site" {
-  bucket = "my-static-site-vidhun-roshan"
+  bucket = "my-unique-static-site-bucket-12345"
 }
 
 resource "aws_s3_bucket_website_configuration" "website" {
@@ -17,4 +17,27 @@ resource "aws_s3_object" "index" {
   content_type = "text/html"
 }
 
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket = aws_s3_bucket.static_site.id
 
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_policy" {
+  bucket = aws_s3_bucket.static_site.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = "s3:GetObject"
+        Resource = "${aws_s3_bucket.static_site.arn}/*"
+      }
+    ]
+  })
+}
